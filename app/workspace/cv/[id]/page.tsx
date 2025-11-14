343a8620d8be7740e86277d73863857ba15b4ab0
Caleb Ganza <icaleb130@gmail.com>
2025-11-14 14:07:05 +0200

    fix: removing unused deps

--- app/workspace/cv/[id]/page.tsx
+++ app/workspace/cv/[id]/page.tsx
@@ -1,8 +1,7 @@
 import { getResume } from "@/app/actions/get-resume";
 import { getStructuredResume } from "@/app/actions/get-structured-resume";
 import { getResumeText } from "@/app/actions/getResumeText";
-import { convertPDF } from "@/app/actions/pdf-to-html";
-import { Room } from "@/app/Room";
+
 import ResumeWorkspace from "@/components/ResumeWorkspace";
 import { notFound } from "next/navigation";
 import React from "react";
@@ -16,11 +15,7 @@ const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
   // const resumeText = await getResumeText(resume.file_url);
   // const finalResume = await getStructuredResume(resumeText);
   // console.log("FINALRESUME", finalResume);
-  return (
-    <Room>
-      <ResumeWorkspace />
-    </Room>
-  );
+  return <ResumeWorkspace />;
 };
 
 export default Page;
